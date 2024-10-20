532486a66736c97cf113a8e8f6f96cfb44a62398
Soupborsh <soupborsh@pc.local>
2024-10-20 19:03:20 +0500

    Add patches to fix Lindroid, change status

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Lindroid-files
-WIP (fails yet)
 
 Build script for LineageOS with Lindroid for POCO X3 NFC (xiaomi-surya). Also contains files needed for script.
+
+## Status
+
+Builds, but Lindroid may not work.
 
 Thanks to Lindroid and LineageOS developers!
--- build.sh
+++ build.sh
@@ -6,10 +6,16 @@ source build/envsetup.sh
 breakfast surya
 wget https://raw.githubusercontent.com/Soupborsh/Lindroid-files/refs/heads/main/lindroid_defconfig.patch
 wget https://raw.githubusercontent.com/Soupborsh/Lindroid-files/refs/heads/main/EventHub.patch
+wget https://github.com/android-kxxt/android_kernel_xiaomi_sm8450/commit/ae700d3d04a2cd8b34e1dae434b0fdc9cde535c7.patch
+wget https://raw.githubusercontent.com/Soupborsh/Lindroid-files/refs/heads/main/0001-Ignore-uevent-s-with-null-name-for-Extcon-WiredAcces.patch
 patch kernel/xiaomi/surya/arch/arm64/configs/surya_defconfig lindroid_defconfig.patch
 patch frameworks/native/services/inputflinger/reader/EventHub.cpp EventHub.patch
+patch kernel/xiaomi/surya/fs/overlayfs/util.c ae700d3d04a2cd8b34e1dae434b0fdc9cde535c7.patch
+git apply 0001-Ignore-uevent-s-with-null-name-for-Extcon-WiredAcces.patch --directory=frameworks/base/
 rm lindroid_defconfig.patch
 rm EventHub.patch
+rm ae700d3d04a2cd8b34e1dae434b0fdc9cde535c7.patch
+rm 0001-Ignore-uevent-s-with-null-name-for-Extcon-WiredAcces.patch
 echo '' >> device/xiaomi/surya/BoardConfig.mk
 echo '# Set SELinux to permissive' >> device/xiaomi/surya/BoardConfig.mk
 echo 'BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive' >> device/xiaomi/surya/BoardConfig.mk
